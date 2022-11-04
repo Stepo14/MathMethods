13320c12a25ada46e575f1dc5edb4e4753f3c061
Stephanie Howell <102824003+Stepo14@users.noreply.github.com>
2022-11-03 23:39:03 -0600

    Update Stephanie_Howell_Project1_Problem2_Draft.py
    Should be returning proper values

--- Stephanie_Howell_Project1_Problem2_Draft.py
+++ Stephanie_Howell_Project1_Problem2_Draft.py
@@ -5,6 +5,7 @@ Created on Thu Nov  3 21:06:09 2022
 @author: Steph
 """
 import math
+import numpy as np
 
 # we are given a rod of given length y1 to y2
 # let's attribute an arbitrary value for the length
@@ -22,20 +23,20 @@ lamb_0 = .5 * 10**-6
 
 # we can define our distance from delta_q to the point of interest to be r
 # our point of interest can be
-P_x = 4
-P_y = 6
+r_p = np.array([4, 6])
 
 # we'll set some initial values to begin our for loop iteration
 summation = 0
 y = 0
 # therefore, r will be
-for y in range(L):
-    # distance formula to find r for each point along the rod
-    r = math.sqrt((P_x)**2 + (P_y - y)**2)
+for y_1 in range(y_2):
+    r_l = np.array([0, y_1])
+    r = r_l - r_p
+    r = math.sqrt(((r[0])**2 + (r[1])**2))
     # our summation to find the integral numerically
     summation = summation + delta_L / r
-    y = y + delta_L
+    y_1 = y_1 + delta_L
 
 # use the formula to calculate the electric potential
 v = (lamb_0 / (4 * math.pi * (8.8 * 10**-12))) * summation
-print(v)
+print("The electric potential is equal to:", v, "Volts")
